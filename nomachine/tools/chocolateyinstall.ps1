88491b8f648cbeb1cc6c1439c4672d887135ec75
Laurent Almeras <lalmeras@gmail.com>
2017-05-21 19:11:28 +0200

    NoMachine (5.2.21.1.0) initial version

---
+++ nomachine/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'nomachine'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://download.nomachine.com/download/5.2/Windows/nomachine_5.2.21_1.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'NoMachine*'
+  checksum      = 'E8109276FA6D06E987C17F637388E58DD4E2A42628D4A291B305D5E6C1A2D850'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
