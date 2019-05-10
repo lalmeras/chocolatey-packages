9df73d6b350f80b7646b7e4fa4f20536023a55e8
Laurent Almeras <lalmeras@gmail.com>
2019-05-10 22:00:15 +0200

    nomachine 6.6.8.501
    nomachine 6.6.8.500 triggers an error window when Print Spooler
    Windows Service is disabled.
    
    This new packaging for 6.6.8_5 version auto-enables Print Spooler
    service during installation, as suggested by gep13 (package reviewer).
    
    This new packaging also adds suggested attributes for package:
    iconUrl (from jsdelivr) and licenseUrl (from nomachine.com website).

--- nomachine/tools/chocolateyinstall.ps1
+++ nomachine/tools/chocolateyinstall.ps1
@@ -4,6 +4,23 @@ $packageName= 'nomachine'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'http://download.nomachine.com/download/6.6/Windows/nomachine_6.6.8_5.exe'
 
+# Make sure Print Spooler service is up and running
+# stolen from hp-universal-print-driver-pcl/cutepdf package.
+try {
+  $serviceName = 'Spooler'
+  $spoolerService = Get-WmiObject -Class Win32_Service -Property StartMode,State -Filter "Name='$serviceName'"
+  if ($spoolerService -eq $null) { throw "Service $serviceName was not found" }
+  Write-Host "Print Spooler service state: $($spoolerService.StartMode) / $($spoolerService.State)"
+  if ($spoolerService.StartMode -ne 'Auto' -or $spoolerService.State -ne 'Running') {
+    Set-Service $serviceName -StartupType Automatic -Status Running
+    Write-Warning 'Print Spooler service new state: Auto / Running'
+    Write-Warning 'NoMachine install is broken if Print Spooler is disabled'
+    Write-Warning 'see https://twitter.com/LaurentAlmeras/status/1126175028993769482'
+  }
+} catch {
+  throw "Unexpected error while checking Print Spooler service: $($_.Exception.Message)"
+}
+
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
